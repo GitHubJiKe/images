dccf357b3d62b0db3dc3c492fefbbb64b083298b
yuanpengfei03 <yuanpengfei03@meituan.com>
2024-02-27 11:51:05 +0800

    add static docs files

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+function genImgList() {
+    const fs = require('fs');
+    const path = require('path');
+
+    const directoryPath = path.join(__dirname, '../uPic');
+    const filePath = path.join(__dirname, '../docs/images.json');
+    fs.readdir(directoryPath, function (err, files) {
+        // 错误处理
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        }
+        // 文件处理
+        fs.writeFile(filePath, JSON.stringify(files), 'utf8', (err) => {
+            if (err) {
+                console.error('An error occurred:', err);
+                return;
+            }
+            console.log('The file has been saved!');
+        });
+    });
+
+}
+
+genImgList()
